b0896776a711ad72785a0de8c4ec32626552c030
Parth Ahluwalia <parthxv@ccs.neu.edu>
2015-02-16 03:24:54 -0500

    Go again

---
+++ experiments/week4/js/experiment3.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("HelloWorldApp", []);
+
+app.controller("HelloWorldController", 
+    function ($scope) {
+        var emp = {
+            first: "Vincent",
+            last: "Chase"
+        };
+
+        $scope.empName = emp;
+
+        $scope.hello = "Hello again from Angular JS";
+
+    });
