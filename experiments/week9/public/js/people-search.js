70ad61f059fb66d83aac4bbe9144f7b39a09da39
Parth Ahluwalia <parthxv@ccs.neu.edu>
2015-04-07 16:23:36 -0400

    Week 9 Experiment 1

---
+++ experiments/week9/public/js/people-search.js
@@ -0,0 +1,81 @@
+﻿
+//People search controller
+app.controller('PeopleCtrl', function ($scope, $http, $window, $rootScope, $location) {
+
+    var userFound = [];
+    //Search a list of users - users/search and their statuses
+    $scope.searchPeople = function (userTerm) {
+        $http.get("/user-search/" + userTerm)
+        .success(function (response) {
+            //console.log(response);
+            userFound = response;
+            $scope.searchedUsers = response;
+        });
+        /* UserTimeline implemented below...
+        var usersTimeline = [];
+        for (var i in userFound) {       
+            //Get user's statuses
+            $http.get("/statuses-user_timeline/" + userFound[i].screen_name)
+            .success(function (statuses) {
+                console.log(statuses);
+                var userPageDetails = {
+                    userDetails: userFound[i],
+                    statuses: statuses
+                };
+                //$scope.userTimeLine = userPageDetails;
+                usersTimeline.push(userPageDetails);
+            });
+        }
+        console.log(usersTimeline);
+
+        $scope.usersTimeline = usersTimeline;
+        */
+    };
+
+    /* Revised code written below!
+    //Show user - users/show
+    $scope.showUserTimeline = function (user) {
+        //var userIdScreenName = user.id + "&&" + user.screen_name;
+        $http.get("/statuses-user_timeline/" + user.screen_name)
+            .success(function (statuses) {
+                //console.log(statuses);
+                var userPageDetails = {
+                    userDetails: user,
+                    statuses: statuses
+                };
+                $scope.userTimeline = userPageDetails;
+                console.log(userPageDetails);
+
+                //Commenting the User Service for now...
+                //Save the user details to be retrieved at other page
+                //userService.setUserDetails(userPageDetails);
+                //$window.location.href = '../views/user-page.html';
+            });
+    };
+    */
+
+    //Show the clicked user's profile
+    $scope.showUserProfile = function (user) {
+
+        //Render the received profile pic to a bigger one!
+        var imgURL = user.profile_image_url;
+        var profile_img_400x400 = imgURL.substring(0, imgURL.length - 12) + "_400x400.jpeg";
+
+        $http.get("/statuses-user_timeline/" + user.screen_name)
+         .success(function (statuses) {
+             //console.log(statuses);
+             var userPageDetails = {
+                 userDetails: user,
+                 statuses: statuses,
+                 profile_img: profile_img_400x400
+             };
+             $rootScope.selectedUserTimeline = userPageDetails;
+             console.log(userPageDetails);
+
+             $location.path('/profile/' + user.screen_name);
+
+         });
+    }
+
+});
+
---
+++ experiments/week9/public/js/phrase-sentiment.js
@@ -0,0 +1,79 @@
+﻿var app = angular.module("SentimentAnalysisApp", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/', {
+            templateUrl: '../views/home.html',
+        }).
+        when('/parallax1', {
+            templateUrl: '../views/phrase-module.html',
+            controller: 'PhraseSentimentCtrl'
+        }).
+        when('/people', {
+            templateUrl: '../views/people.html',
+        }).
+        when('/contact', {
+            templateUrl: '../views/contact.html',
+        }).
+        when('/account', {
+            templateUrl: '../views/account.html',
+            controller: 'UserCtrl'
+        }).
+        when('/profile/:screen_name', {
+            templateUrl: '../views/profile.html',
+            controller: 'UserCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+  }]);
+
+
+//Subject Sentiment Controller
+app.controller("PhraseSentimentCtrl", function ($scope, $http) {
+    $scope.analyzeTweets = function (searchText) {
+        $http.get("/phrase-sentiment/" + searchText)
+            .success(function (tweetSentiment) {
+                //Set scope variable to show the evaluated sentiment results
+                $scope.showSubjectSentiment = true;
+
+                //Generate a comparitive score for the tweets fetched
+                console.log(tweetSentiment);
+                var comparativeScore = 0;
+                for (var i in tweetSentiment) {
+                    comparativeScore += tweetSentiment[i].sentiment.comparative;
+                }
+                comparativeScore = comparativeScore / tweetSentiment.length;
+                
+                $scope.comparativeScore = Math.round(comparativeScore * 1000) / 1000;
+                $scope.tweetSentiments = tweetSentiment;
+
+                //Generate a phrase sentiment corresponding to the comparative score generated for the tweets fetched
+                var comparative = parseFloat(comparativeScore);
+
+                if (0 < comparative && comparative < 0.15) {
+                    $scope.phraseSentiment = "fine"
+                } else if (0.15 <= comparative && comparative < 0.3) {
+                    $scope.phraseSentiment = "incredible"
+                } else if (0.3 < comparative && comparative < 0.6) {
+                    $scope.phraseSentiment = "fantabulous"
+                } else if (comparative >= 0.6) {
+                    $scope.phraseSentiment = "spectacularly out of the world"
+                } else if (comparative === 0) {
+                    $scope.phraseSentiment = "okay"
+                } else if (-0.1 < comparative && comparative < 0) {
+                    $scope.phraseSentiment = "not that good"
+                } else if (-0.25 < comparative && comparative <= -0.1) {
+                    $scope.phraseSentiment = "lousy"
+                } else if (-0.55 < comparative && comparative <= 0.25) {
+                    $scope.phraseSentiment = "awful"
+                } else if (comparative <= -0.55) {
+                    $scope.phraseSentiment = "awfully bad"
+                }
+            });
+    };
+});
+
+
+
